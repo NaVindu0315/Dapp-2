7a4e4899c9f3388f80356b26e14954c606f10836
NaVindu LakShan <89694202+NaVindu0315@users.noreply.github.com>
2024-03-13 13:19:13 +0530

    worked now eth balance is retried

--- lib/home.dart
+++ lib/home.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:http/http.dart'; //You can also import the browser version
+import 'package:web3dart/web3dart.dart';
 
 class Home extends StatefulWidget {
   const Home({Key? key}) : super(key: key);
@@ -8,6 +10,21 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+  var apiUrl =
+      "https://sepolia.infura.io/v3/4887f9655ec94842a2d3206deae69ad2"; //Replace with your API
+
+  var httpClient = Client();
+
+  var credentials = EthPrivateKey.fromHex(
+      "bdb3d39f69282abbce39e6d834b762a6ab093d97b94411f018b1fe607ea017e6");
+
+  Future<void> getbalance() async {
+    var ethClient = Web3Client(apiUrl, httpClient);
+    var address = credentials.address;
+    EtherAmount balance = await ethClient.getBalance(address);
+    print(balance.getValueInUnit(EtherUnit.ether));
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -24,6 +41,11 @@ class _HomeState extends State<Home> {
                     'HI',
                     style: TextStyle(fontSize: 30.0),
                   ),
+                  ElevatedButton(
+                      onPressed: () {
+                        getbalance();
+                      },
+                      child: Text('Test')),
                   Spacer(),
                 ],
               ),

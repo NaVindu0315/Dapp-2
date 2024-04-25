941805603acf3d3dbcd753e1f72ca7da04b62ff5
NaVindu LakShan <89694202+NaVindu0315@users.noreply.github.com>
2024-04-25 19:23:52 +0530

    Update home.dart

--- lib/home.dart
+++ lib/home.dart
@@ -16,22 +16,7 @@ class Home extends StatefulWidget {
   State<Home> createState() => _HomeState();
 }
 
-class _HomeState extends State<Home> {
-  var apiUrl =
-      "https://sepolia.infura.io/v3/4887f9655ec94842a2d3206deae69ad2"; //Replace with your API
 
-  var httpClient = Client();
-
-  var credentials = EthPrivateKey.fromHex(
-      "bdb3d39f69282abbce39e6d834b762a6ab093d97b94411f018b1fe607ea017e6");
-
-  ///to get
-  // Replace with placeholders (refer to security note above)
-  final String infuraEndpoint =
-      'https://sepolia.infura.io/v3/4887f9655ec94842a2d3206deae69ad2'; // Replace with actual endpoint
-  final String infuraApiKey =
-      '4887f9655ec94842a2d3206deae69ad2'; // Replace with actual API key
-  final String contractAddress = '0xc691a5f193883bE1Ef4d03f0c7f60De8B88913A3';
 
   Future<void> getbalance() async {
     var ethClient = Web3Client(apiUrl, httpClient);
